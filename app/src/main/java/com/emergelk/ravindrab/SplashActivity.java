72acf9958e1eb44b7f200d7aa0b1e710783602c4
prabod <praboderathnayaka@gmail.com>
2016-01-02 15:04:05 +0530

    Almost

--- app/src/main/java/com/emergelk/ravindrab/SplashActivity.java
+++ app/src/main/java/com/emergelk/ravindrab/SplashActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by prabod on 1/2/16.
- */
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
